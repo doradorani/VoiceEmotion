d3e9871b3d76c29e76cdab4960f222dd58f1adae
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-04-18 15:09:01 +0900

    model add

--- board/models.py
+++ board/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.utils.timezone import now
+from django.conf import settings
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Board(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256, null=False)
+    content = models.TextField(null=False)
+    image = models.FileField(null = True, blank = True, upload_to='img/')
+    date = models.DateTimeField(default=now, editable=False, null=False)
+    class Meta:
+        db_table = 'board'
+
+class Comment(models.Model):
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=now, editable=False, null=False)
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'comment'
--- member/forms.py
+++ member/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
-
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label='이메일')
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ("username","email")
