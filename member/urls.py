b79588efd6c18c967bcb201c0c433592766cffbe
fish895623 <dan990429@gmail.com>
2022-04-18 16:40:16 +0900

    Resizing

--- member/urls.py
+++ member/urls.py
@@ -6,14 +6,11 @@ from . import views
 app_name = 'member'
 
 urlpatterns = [
-    path('login/',
-         auth_views.LoginView.as_view(
-             template_name='member/login.html'),
-         name='login'
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='member/login.html'),
+        name='login',
     ),
-    path('logout/',
-         auth_views.LogoutView.as_view(),
-         name='logout'
-    ),
-    path('signup/',views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
