a8b51e04eeb7617bd3fecb92dab2e073eddaf724
fish895623 <dan990429>
2022-04-19 16:17:01 +0900

    Isort oitnb

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('board/', views.boardpaging, name='board'),
-    path('write/',views.board_write, name ='write'),
-    path('detail/<int:pk>/',views.board_detail, name = 'board_detail'),
+    path('write/', views.board_write, name='write'),
+    path('detail/<int:pk>/', views.board_detail, name='board_detail'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
+
 from .forms import BoardWriteForm, CommentForm
 from .models import *
 
@@ -39,11 +38,12 @@ def board_write(request):
 
     context = {'form': form}
     return render(request, 'board/write.html', context)
-    
+
+
 @csrf_exempt
 def board_detail(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    comments = Comment.objects.filter(board = pk)
+    comments = Comment.objects.filter(board=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -54,13 +54,8 @@ def board_detail(request, pk):
             return redirect('board:board_detail', pk)
     else:
         form = CommentForm()
-    
-    context = {
-        'form' : form,
-        'board': board,
-        'comments':comments,
-        'pk':pk
-    }
+
+    context = {'form': form, 'board': board, 'comments': comments, 'pk': pk}
     board.hit_cnt += 1
     board.save()
 
