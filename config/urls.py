5e10b302cc5d2f7cc098439b530e5f76b1defcca
fish895623 <dan990429>
2022-04-19 15:34:20 +0900

    Remove sklearn, reformat

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from sklearn.neighbors import NearestCentroid
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('member/', include('member.urls')),
-    path('',include('board.urls'),name='board'),
+    path('', include('board.urls'), name='board'),
 ]
