4dd3cfda2a8fff335ff25bf065c4f428e7646375
fish895623 <dan990429>
2022-04-19 16:38:47 +0900

    Typo

--- board/urls.py
+++ board/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    path('board/', views.boardpaging, name='board'),
+    path('board/', views.board_paging, name='board'),
     path('write/', views.board_write, name='write'),
     path('detail/<int:pk>/', views.board_detail, name='board_detail'),
 ]
