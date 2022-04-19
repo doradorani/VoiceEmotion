f39fb4c12ae5afc9b1452e4dd199c6260bb71413
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-04-19 15:58:47 +0900

    board detail

--- board/urls.py
+++ board/urls.py
@@ -5,4 +5,5 @@ app_name = 'board'
 urlpatterns = [
     path('board/', views.boardpaging, name='board'),
     path('write/',views.board_write, name = 'write'), # 게시글 작성
+    path('detail/<int:pk>/',views.board_detail, name = 'board_detail'),
 ]
