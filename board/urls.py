ba584a7b78e1357bb05c2bf639b80c4f7791b431
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-04-20 14:40:18 +0900

    board edit1

--- board/urls.py
+++ board/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    path('board/', views.board_paging, name='board'),
+    path('', views.board_paging, name='board'),
     path('write/', views.board_write, name='write'),
     path('detail/<int:pk>/', views.board_detail, name='board_detail'),
+    path('detail/edit/<int:pk>/',views.board_edit,name='edit'),
 ]
--- board/views.py
+++ board/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
+from sqlalchemy import null
 
 from .forms import BoardWriteForm, CommentForm
 from .models import *
@@ -87,3 +88,16 @@ def comment(request, board_id):
 
     context = {'board': board, 'form': form}
     return render(request, 'board:detail', context)
+
+@csrf_exempt
+@login_required
+def board_edit(request, pk):
+    board = Board.objects.get(id=pk)
+    if request.method == "POST":
+        board.title = request.POST['title']
+        board.content = request.POST['content']
+        board.save()
+        return redirect('board:board')
+    else:
+        boardForm = BoardWriteForm
+        return render(request, 'board/edit.html', {'boardForm': boardForm})
