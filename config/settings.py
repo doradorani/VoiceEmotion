150e37b9efcea126e79e463855c93b2abaa88afb
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-04-21 11:42:28 +0900

    login fix

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/member/login/'
+LOGIN_REDIRECT_URL = '/main/'
 LOGOUT_REDIRECT_URL = '/member/login/'
 AUTH_USER_MODEL = 'member.User'
