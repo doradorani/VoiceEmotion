55bcbdc3801e02181b2cb1b6ce2e750e6a8b41f5
fish895623 <dan990429@gmail.com>
2022-04-27 22:13:01 +0900

    Add Note, Documentation

--- backend/app.py
+++ backend/app.py
@@ -27,14 +27,9 @@ app = Flask(__name__)
 
 def audio_preprocessing(filename: str) -> list:
     """오디오 전처리"""
-    file_full: str = UPLOAD_DIRECTORY + filename
+    file_path = webm_2_wav(filename=filename)
 
-    # NOTE 다음에 webm_2_wav 함수로 교체예정
-    audioSegment = AudioSegment.from_file(file_full)
-    new_file_path = file_full.replace('webm', 'wav')
-    audioSegment.export(new_file_path, format='wav')
-
-    xf, _ = librosa.load(path=file_full)
+    xf, _ = librosa.load(path=file_path)
     mfcc_1 = librosa.feature.mfcc(y=xf, sr=16000, n_mfcc=5, n_fft=400, hop_length=160)
     mfcc_1 = scale(mfcc_1, axis=1)
     feature = np.mean(mfcc_1.T, axis=0)
@@ -44,17 +39,22 @@ def audio_preprocessing(filename: str) -> list:
 
 # TODO 파일을 저장하고 불러와서 다시 변환하고 삭제하는 형태이기에 저장장치에 무리를 줌
 # TODO fs 모듈을 이용하여 해결하기를 권장
-def webm_2_wav(filename: str) -> None:  # TODO
+def webm_2_wav(filename: str) -> str:
     """WebM to Wav
 
     MediaEncoder에서 받으면 형식이 WebM 형식으로 받아짐.
     librosa에서 처리가 불가하므로 wav로 변환후 처리.
+
+    Return: 
+        str: filename
     """
     file_full: str = UPLOAD_DIRECTORY + filename
     audioSegment = AudioSegment.from_file(file_full)
     new_file_path = file_full.replace('webm', 'wav')
     audioSegment.export(new_file_path, format='wav')
     os.remove(file_full)
+
+    return
 
 
 def audio_predict(x) -> int:
@@ -64,6 +64,7 @@ def audio_predict(x) -> int:
     return result[0].tolist()
 
 
+# NOTE How about using `fs` save on ram and then convert?
 @app.route('/receive', methods=['POST'])
 def form() -> Response:
     """Get wav file"""
@@ -79,6 +80,7 @@ def form() -> Response:
         return jsonify({'status': 'fail'})
 
 
+# NOTE This page is not need
 @app.route('/', methods=['GET'])
 def hello() -> str:
     """Render html"""
