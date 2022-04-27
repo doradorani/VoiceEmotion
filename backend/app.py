b651a477fc79c8d2da389dd73e34473923d9de65
fish895623 <34264960+fish895623@users.noreply.github.com>
2022-04-27 10:10:46 +0900

    api 제작완료 검증부탁 (#19)
    * a
    
    * Update requirements
    
    * Remove Stagefile
    
    * Recover
    
    * new Date() is not valid for python
    
    * Add opening tag

---
+++ backend/app.py
@@ -0,0 +1,59 @@
+import os
+
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import joblib
+import numpy as np
+import librosa
+from sklearn.preprocessing import scale
+import warnings
+
+warnings.filterwarnings("ignore")
+
+model = joblib.load(open("../model/saved_model/model_lgbm.pkl", "rb"))
+label = ['anger', 'angry', 'disgust', 'fear', 'happiness', 'neutral', 'sad', 'surprise']
+
+UPLOAD_DIRECTORY = './tmp/'
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.mkdir(UPLOAD_DIRECTORY)
+
+app = Flask(__name__)
+
+
+def audio_preprocessing(filename):
+    xf, sr = librosa.load(UPLOAD_DIRECTORY + filename)
+    mfcc_1 = librosa.feature.mfcc(y=xf, sr=16000, n_mfcc=5, n_fft=400, hop_length=160)
+    mfcc_1 = scale(mfcc_1, axis=1)
+    feature = np.mean(mfcc_1.T, axis=0)
+    return [feature]
+
+
+# 오디오 테스트 데이터 학습
+def audio_test(x_test):
+    result = model.predict(x_test)
+    return result[0].tolist()
+
+
+@app.route('/receive', methods=['POST'])
+def form():
+    """Get wav file"""
+    file = request.files['file']
+    if file.filename is not None:
+        filename = secure_filename(filename=file.filename)
+        file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+        x_test = audio_preprocessing(file.filename)
+        predict_result = audio_test(x_test)
+        return jsonify({'status': 'success', "result": label[predict_result]})
+    else:
+        return jsonify({'status': 'fail'})
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """Render html"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ backend/static/index.js
@@ -0,0 +1,79 @@
+/*********************************
+ getUserMedia returns a Promise
+ resolve - returns a MediaStream Object
+ reject returns one of the following errors
+ AbortError - generic unknown cause
+ NotAllowedError (SecurityError) - user rejected permissions
+ NotFoundError - missing media track
+ NotReadableError - user permissions given but hardware/OS error
+ OverconstrainedError - constraint video settings preventing
+ TypeError - audio: false, video: false
+ post_data - post data to flask api
+ *********************************/
+let post_address = "/receive";
+let delay = 2000;
+let save_file_format = `${new Date().getTime()}.wav`;
+let constraintObj = {audio: true};
+
+
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+  navigator.mediaDevices.getUserMedia = function (constraintObj) {
+    let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    if (!getUserMedia) {
+      return Promise.reject(new Error("getUserMedia is not implemented in this browser"));
+    }
+    return new Promise(function (resolve, reject) {
+      getUserMedia.call(navigator, constraintObj, resolve, reject);
+    });
+  };
+} else {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
+      devices.forEach((device) => {
+        console.log(device.kind.toUpperCase(), device.label);
+      })
+    })
+    .catch((err) => {
+      console.log(err.name, err.message);
+    });
+}
+
+navigator.mediaDevices
+  .getUserMedia(constraintObj)
+  .then(function (mediaStreamObj) {
+    let start = document.getElementById('recordStart');
+    let mediaRecorder = new MediaRecorder(mediaStreamObj);
+    let chunks = [];
+
+    start.addEventListener("click", () => {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+      setTimeout(() => {
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+      }, delay)
+    })
+    mediaRecorder.ondataavailable = function (ev) {
+      chunks.push(ev.data);
+    }
+    mediaRecorder.onstop = () => {
+      let blob = new Blob(chunks, {'type': 'audio/wav;'});
+      post_data(blob)
+      chunks = [];
+    }
+  })
+  .catch(function (err) {
+    console.log(err.name, err.message);
+  });
+
+function post_data(blob) {
+  const fd = new FormData();
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("POST", post_address, false);
+  fd.append("file", blob, save_file_format);
+  xhr.send(fd);
+  console.log(xhr.responseText);
+}
