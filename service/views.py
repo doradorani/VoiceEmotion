c2be3ea4a058990ca156fe57b9a0b8aab70a555e
doradorani <sokoyo2@gmail.com>
2022-05-03 16:37:42 +0900

    김란희/회원가입 색상 클릭 시 수정, 서비스앱 필요없는 부분 삭제, 로고 임시 변경

--- service/views.py
+++ service/views.py
@@ -6,16 +6,3 @@ from django.views.decorators.csrf import csrf_exempt
 
 def service(request) -> HttpResponse:
     return render(request, "index.html", {})
-
-def chat(request):
-    return render(request, 'service/chat.html')
-
-@csrf_exempt
-def chatbot(request):
-    get_data = json.loads(request.body.decode('utf-8'))
-    print(get_data['message'])
-    data = {
-        'message':'안녕하세요.'
-
-    }
-    return JsonResponse(data)
