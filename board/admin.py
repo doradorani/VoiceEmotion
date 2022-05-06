7086b3c7bb489b15f48a3ffec2b8504854cf8e6d
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-05-06 14:28:49 +0900

    comment admin

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Board, Notice
+from .models import Board, Notice,Comment
 
 admin.site.register(Notice)
 admin.site.register(Board)
+admin.site.register(Comment)
