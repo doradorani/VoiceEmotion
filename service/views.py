4ff506d9a646d647bcacfb5bd29d6344d1afc980
doradorani <sokoyo2@gmail.com>
2022-05-06 14:17:32 +0900

    김란희/서비스화면

--- service/views.py
+++ service/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def service(request) -> HttpResponse:
-    username = request.user
+    username = request.user.id
     response = render(request, "index.html", {})
-    response.set_cookie('username',username)
+    response.set_cookie('user_id',username)
     return response
     
