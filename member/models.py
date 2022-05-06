f3a22d7dc685bf11d4b28d583f2f57f72044fea0
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-05-06 14:36:12 +0900

    오타수정

--- member/models.py
+++ member/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    GENDER_CHOICES = (('male', 'mail'), ('female', 'female'))
+    GENDER_CHOICES = (('male', 'male'), ('female', 'female'))
 
     genres = models.CharField(max_length=50)
     gender = models.CharField(default='male',max_length=10, choices=GENDER_CHOICES)
