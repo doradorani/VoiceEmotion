6dcb3e1af321f3147cd0df8ab7a2e1b057c66fd7
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-05-10 13:29:30 +0900

    main data

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from board.models import Movie
+from board.models import Movie,Ratings
 
 def main(request) -> HttpResponse:
     movie = Movie.objects.order_by('movieId')[:10]
-    context = {'movie':movie}
+    rating = Ratings.objects.raw('SELECT avg(rating) as avg FROM Ratings GROUP BY movieId ORDER BY movieId ASC')
+    context = {'movie':movie,'rating':rating}
     return render(request, 'main.html', context)
 
 def introduction(request) -> HttpResponse:
