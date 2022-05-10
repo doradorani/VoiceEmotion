7d8f167571377c7b53d8435be54a8f2d01e2a43e
dlatjdgus955 <dlatjdgus955@gmail.com>
2022-05-10 10:32:45 +0900

    main 수정

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from board.models import Movie
 
 def main(request) -> HttpResponse:
-    return render(request, 'main.html', {})
+    movie = Movie.objects.all()
+    return render(request, 'main.html', {'movie',movie})
 
 def introduction(request) -> HttpResponse:
     return render(request, 'introduction.html', {})
